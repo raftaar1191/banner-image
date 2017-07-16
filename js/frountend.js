f3a4897fbfc7ca8e2970c28fb63ff05b293b0879
Deepak Gupta <raftaar1191@gmail.com>
2017-07-16 20:11:51 +0530

    Add JS #6
    Add js to add class when user scroll on the sites

--- js/frountend.js
+++ js/frountend.js
@@ -1,4 +1,5 @@
 var $ = jQuery;
+var class_add = false;
 (function(){
     $( document ).ready( function( $ ){
         $( 'div.banner-image-wp-head' ).on( 'click', '.banner-close', function( e ){
@@ -6,6 +7,13 @@ var $ = jQuery;
             $( this ).closest( 'div.banner-image-wp-head' ).fadeOut( 500, function(){
                 $( this ).remove();
             });
+        });
+
+        $( document ).scroll( function() {
+            if( false === class_add ) {
+                $( 'div.banner-image-wp-head' ).addClass( 'banner-image-fixed' );
+                class_add = true;
+            }
         });
     });
 })();
